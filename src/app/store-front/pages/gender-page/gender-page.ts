1d9bf24dcb4bd7cd952e491ddc4a4af407b13dd5
juan manuel <jmgv87191@gmail.com>
2025-08-21 12:51:40 -0700

    asd

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { Products } from '../../../products/services/products';
+import { ProductResponse } from '../../../products/interfaces/product.interface';
+import { ProductCard } from '../../components/product-card/product-card';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ ProductCard ],
   templateUrl: './gender-page.html',
   styleUrl: './gender-page.css'
 })
 export class GenderPage {
+
+  route = inject( ActivatedRoute )
+  private productsService = inject(Products);
+
+  gender = toSignal(
+    this.route.params.pipe(
+      map( ({ gender })=> gender )
+    )
+  )
+
+
+  productResource = rxResource<ProductResponse, { limit?: number; gender?: string }>({
+    params: () => ({ limit: 9, gender: this.gender() }),
+    stream: ({ params }) => this.productsService.getProducts(params),
+  });
 
 }
