f26d8faec12433b7a828d1a8d6b07c03ad45ed62
juan manuel <jmgv87191@gmail.com>
2025-09-30 08:12:54 -0700

    asd

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductResponse } from '../../../products/interfaces/product.interface';
+import { Products } from '../../../products/services/products';
+import { PaginationService } from '../../../shared/components/pagination/pagination.service';
+
+type QueryParams = { limit?: number; gender?: string; page?: number; offset?: number };
+
 
 @Component({
   selector: 'app-product-admin-page',
@@ -7,5 +14,19 @@ import { Component } from '@angular/core';
   styleUrl: './product-admin-page.css'
 })
 export class ProductAdminPage {
+  
+  private productsService = inject(Products);
+  paginationService = inject(PaginationService)
+
+
+  productResource = rxResource<ProductResponse, QueryParams>({
+    params: () => {
+      const limit = 9;
+      const page = this.paginationService.currentPage();
+      const offset = (page - 1) * limit;
+      return { limit, gender: '', page, offset };
+    },
+    stream: ({ params }) => this.productsService.getProducts(params), 
+  });
 
 }
