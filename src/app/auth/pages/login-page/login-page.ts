2ef545a07cbe634669f88689a8baca3d7dc5a9df
Juan manuel gutierrez villegas <jmgv87191@gmail.com>
2026-02-25 16:29:16 -0700

    register page

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ReactiveFormsModule,FormBuilder, Validators  } from "@angular/forms";
+import { RouterLink } from "@angular/router";
+
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterLink,ReactiveFormsModule ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(true);
+
+  loginform = this.fb.group({
+    email:[ '',[Validators.required, Validators.email] ]
+  })
 
 }
